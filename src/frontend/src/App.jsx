1f8b4de596b525ffdd3738602246b144f2267940
nico.koltermann <nico.koltermann@tu-dortmund.de>
2025-06-16 10:19:01 +0200

    Layout page

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -2,22 +2,48 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { loadParties } from './utils/loadParties';
 import PartyCard from './components/PartyCard';
+import MenuBar from './components/MenuBar';
+import GenAIBox from './components/GenAIBox';
+import ScrollToTopButton from './components/ScrollToTopButton';
+import PromptDescription from './components/Description';
+import Footer from './components/Footer';
+import Disclaimer from './components/Disclaimer';
+
 
 function App() {
     const [parties, setParties] = useState([]);
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
     useEffect(() => {
         loadParties().then(setParties);
     }, []);
 
     return (
-        <div className="min-h-screen p-4 bg-gray-50">
-            <h1 className="text-3xl font-bold text-center mb-8">Dortmund Kommunalwahl</h1>
-            <div className="max-w-6xl mx-auto">
-                {parties.map((party, index) => (
-                    <PartyCard key={index} party={party} />
-                ))}
+        <div className='w-full flex justify-center items-center flex-col'>
+            <MenuBar />
+            <ScrollToTopButton />
+            <div className="flex justify-center flex-col max-w-3xl">
+                <GenAIBox selectedImageIndex={selectedImageIndex} setSelectedImageIndex={setSelectedImageIndex} />
+                <div className='border-t-2 border-secondary my-10'>
+                    <h1 className='text-secondary m-5'>Wahlprogramme 2025</h1>
+                </div>
+                <div className="max-w-4xl mx-auto m-4">
+                    {parties.map((party, index) => (
+                        <PartyCard
+                            key={index}
+                            party={party}
+                            align={index % 2 === 0 ? 'left' : 'right'}
+                            selectedImageIndex={selectedImageIndex}
+                        />
+                    ))}
+                </div>
+                {/* <div className='border-t-4 border-secondary my-10'>
+                    <h1 className='text-secondary m-5'>Anfrage an die KI</h1>
+                </div> */}
+                {/* <PromptDescription></PromptDescription> */}
+                <Disclaimer></Disclaimer>
             </div>
+            <Footer></Footer>
         </div>
     );
 }
