c52bff6e1d983b9fc08b3f28a814f2bcc47bb7a3
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2025-07-23 08:41:01 +0000

    small fixes

--- generate_multi_img.sh
+++ generate_multi_img.sh
@@ -1,14 +1,10 @@
 #! /bin/bash
 
-# python3 src/main.py --input contents/2025
-
+# pdf programs
 for p in "contents/2025/cdu/program.pdf" "contents/2025/spd/program.pdf" "contents/2025/linke/program.pdf" "contents/2025/gruene/program.pdf" "contents/2025/fdp/program.pdf"
 do
-    # for b in "contents/reinoldi.jpg" "contents/Herunterladen.jpg" "contents/stadtpanorama_volkswohlbund_01_roland_gorecki_1.jpg"
-    # do
-    #     for g in "1" "10" "100" "1000" "10000"
-    #     do
     python3 src/main.py --input $p
-    #     done
-    # done
 done
+
+# kommunalomat programs
+python src/main.py
