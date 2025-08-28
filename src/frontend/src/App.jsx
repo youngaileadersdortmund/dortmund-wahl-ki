264bf1c8a1bfa6eba4ff03f2a9e7bee842975bc7
nico.koltermann <nico.koltermann@tu-dortmund.de>
2025-08-28 16:45:14 +0200

    Change paths

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -9,7 +9,7 @@ import TechPage from "./pages/TechPage";
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="dortmund-wahl-ki">
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
