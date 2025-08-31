1abe0e3d354fb774f52abea2dab1eb1d2a894318
tareq-kh <46745136+tareq-kh@users.noreply.github.com>
2025-08-31 13:54:56 +0200

    Simplify klaroConfig structure in config.js for debugging
    Removed detailed configurations for services and simplified the klaroConfig structure.

--- src/frontend/config.js
+++ src/frontend/config.js
@@ -1,47 +1,16 @@
-var klaroConfig = {
-    acceptAll: true,
-    services: [
-        {
-            name: 'google-tag-manager',
-            required: true,
-            purposes: ['marketing'],
-            onAccept: `
-                // we notify the tag manager about all services that were accepted. You can define
-                // a custom event in GTM to load the service if consent was given.
-                for(let k of Object.keys(opts.consents)){
-                    if (opts.consents[k]){
-                        let eventName = 'klaro-'+k+'-accepted'
-                        dataLayer.push({'event': eventName})
-                    }
-                }
-            `,
-            onInit: `
-                // initialization code here (will be executed only once per page-load)
-                window.dataLayer = window.dataLayer || [];
-                window.gtag = function(){dataLayer.push(arguments)}
-                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied', 'ad_user_data': 'denied', 'ad_personalization': 'denied'})
-                gtag('set', 'ads_data_redaction', true)
-            `,
-        },
-        {
-            // In GTM, you should define a custom event trigger named `klaro-google-analytics-accepted` which should trigger the Google Analytics integration.
-            name: 'google-analytics',
-            cookies: [
-                /^_ga(_.*)?/ // we delete the Google Analytics cookies if the user declines its use
-            ],
-            purposes: ['marketing'],
-            onAccept: `
-                // we grant analytics storage
-                gtag('consent', 'update', {
-                    'analytics_storage': 'granted',
-                })
-            `,
-            onDecline: `
-                // we deny analytics storage
-                gtag('consent', 'update', {
-                    'analytics_storage': 'denied',
-                })
-            `,
-        },
-    ]
+// config.js
+window.klaroConfig = {
+  version: 2, // bump version to force re-show
+  acceptAll: true,
+  services: [
+    {
+      name: 'google-tag-manager',
+      required: true,
+      purposes: ['marketing'],
+    },
+    {
+      name: 'google-analytics',
+      purposes: ['marketing'],
+    }
+  ]
 }
